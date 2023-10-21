7cee01f8327f37ddb179d694de69a1d257d01d0b
oghene-ella <oghenekaro57@gmail.com>
2023-10-21 19:05:33 +0100

    setup the routes, and server

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
-console.log("hello ella")
+// call all the modules including the config module
+const express = require("express");
+const connectBlogMongo = require("./config/config");
+require("dotenv").config();
+
+
+// port
+const PORT = process.env.PORT;
+
+// create express app
+const app = express();
+
+// use ejs as view engine
+app.set("view engine", "ejs");
+
+// parses incoming requests/data to JSON payloads
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+// static folder path
+app.use("/src", express.static("src"));
+
+// get landing page  route
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.get("/sub", (req, res) => {
+	res.render("sub_page");
+});
+
+// get method for the login page
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+// get method for the sign_up page
+app.get("/signup", (req, res) => {
+	res.render("signup");
+});
+
+
+// get method for the dashboard page
+app.get("/dashboard", (req, res) => {
+	res.render("dashboard");
+});
+
+app.get("/edit", (req, res) => {
+	res.render("edit");
+});
+
+// establish connection to mongodb
+connectBlogMongo.connectBlogMongo();
+
+// listener
+app.listen(PORT, () => {
+	console.log(`server listening at http://localhost:${PORT}`);
+});
