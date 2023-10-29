415f10952497b479aaf1d871ea6ef54c93da7e7d
oghene-ella <oghenekaro57@gmail.com>
2023-10-29 12:10:06 +0100

    made some changes to the views and cookie authentication

--- cookieAuth/Auth.js
+++ cookieAuth/Auth.js
@@ -1,14 +1,18 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const CookieAuth = ( req, res, next) => {
-	const token = req.cookies.jwt
-	console.log(token)
+const CookieAuth = (req, res, next) => {
+	const token = req.cookies.jwt;
+	console.log(token);
 
-	const validToken = jwt.verify(token, process.env.JWT_TOKEN)
-	console.log(validToken)
+	const validToken = jwt.verify(token, process.env.JWT_TOKEN);
+	console.log(validToken);
+
+	res.locals.user = validToken;
+	console.log(res.locals.user);
+
 	next();
-}
+};
 module.exports = {
-	CookieAuth
+	CookieAuth,
 };
