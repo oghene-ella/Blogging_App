11cbdef02cda6daf1054eaa3eb2e782f033b31da
oghene-ella <oghenekaro57@gmail.com>
2023-11-01 10:41:52 +0100

    code formatting, updated the cookie auth, made some changes to the styles

--- cookieAuth/Auth.js
+++ cookieAuth/Auth.js
@@ -2,15 +2,28 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const CookieAuth = (req, res, next) => {
-	const token = req.cookies.jwt;
-	console.log("cookie jwt: ", token)
+	try {
+        const token = req.cookies.jwt;
 
-	const validToken = jwt.verify(token, process.env.JWT_TOKEN);
-
-	res.locals.user = validToken;
-	console.log("user info: ", res.locals.user);
-
-	next();
+        if (!token) {
+            res.cookie("Error when trying to login")
+            res.redirect('/login')
+        }
+        
+        const validToken = jwt.verify(token, process.env.JWT_TOKEN);
+        
+        if (!validToken) {
+			res.cookie("Error when trying to login");
+			res.redirect("/login");
+		}
+        
+        res.locals.user = validToken;
+        next();
+	}
+	catch (error) {
+        res.cookie("Try logging in again!");
+        res.redirect('/login')
+    }
 };
 module.exports = {
 	CookieAuth,
